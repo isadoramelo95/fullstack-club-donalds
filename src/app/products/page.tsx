95f8f7d0173fd5cb6fa6b555370c0a54ec168c4d
isado <isado@LAPTOP-UC5IQEQ6>
2025-02-21 15:07:09 -0300

    feat: add initial setup

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button"
+
+const ProductsPage = () => {
+    return ( <div className="p-5 border border-red-500 rounded-xl">
+        <h1 className="text-red-500">Producst page</h1>
+        <Button>Click me</Button>
+    </div> );
+}
+ 
+export default ProductsPage;
